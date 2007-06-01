ed33d37c5b35951a0f5afdbb381ad7725ce1f34f
bielohla <bielohla@10726908-5857-49ab-a940-16dc6c049dbb>
2007-06-01 23:39:00 +0000

    removed require Tag
    git-svn-id: https://whoami.opendfki.de/repos/trunk@33 10726908-5857-49ab-a940-16dc6c049dbb

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,7 +1,5 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-require_dependency 'Tag'
-
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_whoami2_session_id'
